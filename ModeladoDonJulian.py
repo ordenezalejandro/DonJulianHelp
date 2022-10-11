9df061e97462434b264ca89d7d770ac228667fc2
ordonezalejandro <ordonezgabrielalejandro2@gmail.com>
2022-10-10 21:36:20 -0300

    limpiando repositorio

---
+++ ModeladoDonJulian.py
@@ -0,0 +1,402 @@
+import datetime
+import json
+
+
+class Mueble:
+    def __init__(self,nombre, descripcion,precio,lista_de_piezas = None ,lista_de_extras = None):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        if lista_de_piezas == None :
+            self.lista_de_piezas = []
+        else:
+            self.lista_de_pieza = lista_de_piezas
+        if lista_de_extras == None :
+            self.lista_de_extras = []
+        else:
+            self.lista_de_extras = lista_de_extras
+        self.precio = precio
+        print('mueble_nuevo')
+
+    def agregar_pieza(self,pieza):
+        assert type(pieza) == Pieza
+        self.lista_de_piezas.append(pieza)
+
+    def agregar_extra(self,extra):
+        self.lista_de_extras.append(extra)
+
+    def cantidad_de_piezas(self):
+        return len (self.lista_de_piezas)
+
+    """Crear un método en la clase mueble, que se llame  calcular_despiece,
+    que no tome parámetro, y que devuelve la suma de los pie de cada pieza,
+     (recordar que los piez están en la instancia, con el atributo 
+     lista_de_pieza )"""
+
+    def calcular_despiece(self):
+        """
+        recorrer la lista_de_pieza, calcular_el pie.
+        guardar el valor y sumarlo a la pieza anterior, en una variable.
+        retorne una variable
+
+        """
+        sumatoria_de_pie = 0
+
+        for pieza in self.lista_de_piezas:
+
+            pie = pieza.calcular_pie()
+
+            sumatoria_de_pie = sumatoria_de_pie + pie
+
+        return sumatoria_de_pie
+
+    def calcular_precio_de_piezas(self):
+
+        valor_de_piezas = 0
+
+        for pieza in self.lista_de_piezas:
+
+            precio_de_piezas = pieza.precio
+
+            valor_de_piezas = valor_de_piezas +precio_de_piezas
+
+        return valor_de_piezas
+
+    def calcular_precio_de_extras(self):
+
+        valor_de_extras = 0
+
+        for extra in self.lista_de_extras:
+
+            precio_extra  = extra.precio
+
+            valor_de_extras = valor_de_extras + precio_extra
+
+        return valor_de_extras
+
+    def devolver_costo_de_mueble(self):
+
+        costo_de_mueble = self.calcular_precio_de_piezas() + self.calcular_precio_de_extras()
+
+        return costo_de_mueble
+
+
+        pass
+
+
+class Pieza:
+    def __init__(self,alto,ancho,espesor,descripcion,precio=1):
+        self.alto = alto
+        self.ancho = ancho
+        self.espesor = espesor
+        self.descripcion = descripcion
+        self.precio = precio
+
+    def calcular_pie(self):
+        return (self.alto * self.ancho * self.espesor)/900
+
+
+class Extra:
+    def __init__(self,nombre, descripcion, precio = 0):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.precio = precio
+
+    def cantidad_extra(self):
+        self.cantidad_extra = lista_extra
+        return len(lista_de_piezas)
+
+def total_componentes(lista_de_piezas,lista_extra):
+    return len(lista_de_piezas) +len(lista_extra)
+
+
+lista_de_piezas = []
+estante = Pieza(50,30,2,'estante')
+puerta = Pieza(58,35,2.3,'puerta bajo')
+zocalo = Pieza(7,200,2,'zocalo bajo')
+
+lista_de_piezas.append(estante)
+lista_de_piezas.append(puerta)
+lista_de_piezas.append(zocalo)
+
+lista_extra = []
+bisagra = Extra('bisagra','2 bisagras x puerta')
+tornillos = Extra('tornillos', '8 tornillos x puerta')
+guias = Extra('guias','telescopicas de 45')
+
+lista_extra.append(bisagra)
+lista_extra.append(tornillos)
+lista_extra.append(guias)
+
+cajonera = Mueble('cajonera', '6 cajones',100,lista_de_piezas,lista_extra)
+
+class Cliente:
+
+    def __init__(self, nombre, apellido, edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+
+    def __eq__(self, other):
+       return self.nombre == other.nombre and self.apellido == other.apellido
+
+    def __lt__(self, otro_cliente):
+        return self.edad < otro_cliente.edad
+
+    def __gt__(self,otro_cliente):
+        return self.edad> otro_cliente.edad
+
+class Venta:
+    def __init__(self):
+        self.cliente = None
+        self.mueble = None
+        self.fecha_de_venta = datetime.datetime.now()
+        self.fecha_de_entrega = None
+        self.total = None
+        self.seña = None
+        self.motivo = None
+        self.saldo = None
+
+    def poner_cliente(self, cliente):
+        assert isinstance(cliente, Cliente)
+        self.cliente = cliente
+
+    def poner_mueble(self,mueble):
+        assert isinstance(mueble,Mueble)
+        self.mueble = mueble
+
+    def poner_motivo(self,motivo):
+        assert isinstance(motivo,str)
+        self.motivo = motivo
+
+    def poner_precio(self,precio):
+        assert isinstance(precio,float)
+        self.precio = precio
+
+    def adelanto(self,adelanto):
+        assert isinstance(adelanto, float)
+        self.adelanto = adelanto
+
+    def calcular_saldo(self):
+        return self.precio - self.adelanto
+
+    def guardar_venta(self):
+        with open('registro_de_ventas.json', 'w') as archivo:
+            json.dump(self.listas_de_ventas, archivo)
+
+    def guardar_cargar(self):
+        with open('registro_de_ventas.json', 'r') as archivo:
+            self.lista_de_ventas = json.load(archivo)
+
+
+
+
+class RegistroDeVentas:
+    def __init__(self):
+        self.lista_de_ventas = []
+        self.registro_de_clientes = RegistroDeClientes()
+
+    def agregar_venta(self,venta):
+        assert type(venta) == Venta
+        self.lista_de_ventas.append(venta)
+
+    def devolver_cantidad_ventas(self):
+        return len (self.lista_de_ventas)
+
+
+
+    def devolver_las_ventas_de(self,cliente):
+        coincidencia = []
+
+        for venta in self.lista_de_ventas:
+            if self.venta.cliente.__eq__(cliente):
+                coincidencia.append(venta)
+        return coincidencia
+
+
+
+
+    # recorra la lista de venta-->for
+    # compare q los nombres sean iguales--> ==
+    #si son iguales agregarlos a la lista devolver_ventas_de--> append.devolver_las_ventas_de
+    # retornar esa lista de coincidencias
+
+
+
+    def listar_ventas(self):
+        """definir el methodo listar_ventas, de la clase registro_ventas,
+        que no toma parametro, y que recorre la lista de ventas, e imprime,
+         el nombre y apellido del cleinte, el nombre del mueble,
+         y la fecha_de inicio, la fecha de salida"""
+
+        for venta in self.lista_de_ventas:
+            print('nombre{},apelllido{},mueble{},fecha de venta{},fecha de entrega{}'.format(venta.cliente.nombre,venta.cliente.apellido,venta.mueble,venta.fecha_de_venta,venta.fecha_de_entrega))
+
+    def registrar_venta_input(self):
+        cliente = self.registro_de_clientes.agregar_cliente_input()
+        new_venta = Venta()
+        new_venta.poner_cliente(cliente)
+        mueble = self.registro_de_mueble.agregar_mueble_input()
+        new_venta.poner_mueble(mueble)
+        self.agregar_venta(new_venta)
+
+
+
+
+
+"""agregar el methodo agregar_venta,  a la clase RegistroVenta, 
+que toma como una parametro una instancia de venta, 
+y la agrega a la lista."""
+
+
+class RegistroDeClientes:
+    def __init__(self):
+        self.listas_de_clientes = []
+        self.cargar_cliente()
+
+    def agregar_cliente(self,cliente):
+        assert type(cliente) == Cliente
+        self.listas_de_clientes.append(cliente)
+
+    def agregar_cliente_input (self):
+        apellido = input ('apellido del cliente')
+        nombre = input ('nombre del cliente')
+        edad = input ('ingrese la edad del cliente')
+        if self.existe_cliente(nombre,apellido):
+            cliente = self.obtener_cliente(nombre,apellido)
+
+        else :
+
+            cliente = Cliente (apellido =apellido,nombre = nombre,edad =edad)
+
+            self.agregar_cliente(cliente)
+
+        return cliente
+
+    def existe_cliente(self,nombre,apellido):
+        for cliente in self.lista_de_clientes:
+            if cliente.nombre == nombre and cliente.apellido == apellido:
+                return True
+        return False
+
+
+    def guardar_cliente(self):
+
+        with open ('registro_de_clientes.json', 'w') as archivo:
+            json.dump(self.listas_de_clientes, archivo)
+
+    def cargar_cliente(self):
+
+        with open ('registro_de_clientes.json','r') as archivo:
+
+            self.lista_de_clientes = json.load(archivo)
+
+class Num:
+
+    def __init__(self,numero,lista_de_numeros):
+        self.numero = numero
+        self.lista_de_numeros = lista_de_numeros
+    def sumatoria(self):
+
+        sumatoria_de_numeros = 0
+
+        for numeros in lista_de_numeros:
+
+            numero = num.numero
+
+            sumatoria_de_numeros = sumatoria_de_numeros + numero
+
+        return sumatoria_de_numeros
+
+def sumar_lista_de_numeros(lista_de_numeros):
+
+    sumar = 0
+
+    for numero in lista_de_numeros:
+
+        sumar += numero
+
+    return sumar
+
+def productoria_lista_de_numeros(lista_de_numeros):
+
+    multiplicar = 1
+
+    for numero in lista_de_numeros:
+
+        multiplicar *= numero
+
+    return multiplicar
+
+
+"""ponele si yo te digo, hace una funcion que tome , una lista de 
+lista de numeros, y devuelva una sola lista con todos los numero,
+es decir. aplanar_lista([[1,2,3], [3,4,5], [2,0]])  
+deberia devolver [1,2,3,4,5,2,0]"""
+
+"""
+lista de, lista de numeros,
+agruparlos en una sola lista, con la funcion append 
+"""
+def aplanar_listas(lista_de_lista_de_numeros):
+
+    lista_aplanada = []
+
+    for lista_de_numeros in lista_de_lista_de_numeros:
+
+        lista_aplanada.append(lista_de_numeros)
+
+    return lista_aplanada
+
+"""def dame_mayor, que tome una lista y me devuelva el de mayor valor"""
+
+def dame_mayor(lista):
+
+    mayor = lista[0]
+
+    for numero in lista:
+
+
+        if mayor < numero:
+
+            mayor = numero
+
+    return mayor
+
+"""
+recorrer la lista
+evaluar si es par o impar
+asignarlos a una nueva lista
+devuelva las 2 listas
+"""
+
+def par_o_impar(lista):
+
+    par = []
+    impar = []
+
+    for numero in lista:
+        if numero%2 == 0:
+            par.append(numero)
+        else:
+            impar.append(numero)
+
+    return par , impar
+
+def separar_sub_20(lista_de_personas):
+
+    menor_de_20 = []
+    mayor_20 = []
+
+    for persona in lista_de_personas:
+
+        if persona.edad>20:
+            mayor_20.append(persona)
+        else:
+            menor_de_20.append(persona)
+
+    return menor_de_20, mayor_20
+
+
+
+
+
---
+++ test_practico1.py
@@ -0,0 +1,220 @@
+from ModeladoDonJulian import Mueble,Pieza, Extra,total_componentes, Cliente, Venta, RegistroDeVentas
+from unittest.mock import patch
+from datetime import datetime
+import unittest
+
+class Test_total_de_componente(unittest.TestCase):
+    def test_total_compónente(self):
+        """
+        tomar lista_de_pieza mas lista_extra y me retorna el total de elementos de ambas listas
+        """
+
+        lista_de_piezas = []
+        estante = Pieza(50, 30, 2, 'estante')
+        puerta = Pieza(58, 35, 2.3, 'puerta bajo')
+        zocalo = Pieza(7, 200, 2, 'zocalo bajo')
+
+        lista_de_piezas.append(estante)
+        lista_de_piezas.append(puerta)
+        lista_de_piezas.append(zocalo)
+
+        lista_extra = []
+        bisagra = Extra('bisagra','2 bisagras x puerta')
+        tornillos = Extra('tornillos', '8 tornillos x puerta')
+        guias = Extra('guias','telescopicas de 45')
+
+        lista_extra.append(bisagra)
+        lista_extra.append(tornillos)
+        lista_extra.append(guias)
+        self.assertEqual(6,total_componentes(lista_de_piezas,lista_extra),'no corresponde')
+
+    def test_agregar_pieza(self):
+        cajonera = Mueble('cajonera','6 cajones',precio = 1)
+
+        self.assertEqual(0,len(cajonera.lista_de_piezas))
+        estante = Pieza(2,4,1,'estante cajonera')
+
+        cajonera.agregar_pieza(estante)
+        self.assertEqual(1,len(cajonera.lista_de_piezas), 'no se pudo agregar la pieza, de forma correcta')
+
+    def test_agregar_extra(self):
+        cajonera = Mueble('cajonera','6 cajones',1)
+
+        bisagra = Extra('bisagra','codo 9')
+        cajonera.agregar_extra(bisagra)
+
+        self.assertEqual(1,len(cajonera.lista_extra),'no se agrego a la lista extra')
+
+    def test_mueble(self):
+        cajonera = Mueble('cajonera','6 cajones',2)
+        costado = Pieza(11,22,1,'costado cajon')
+        cajonera.agregar_pieza(costado)
+
+        guias = Extra('guias', 'telescopicas 35cm')
+
+        cajonera.agregar_extra(guias)
+
+        self.assertEqual(2, len(cajonera.lista_de_piezas) + len(cajonera.lista_de_extras), 'no se agregaron las piezas a las listas')
+        extra = cajonera.lista_de_extras[0]
+
+        self.assertEqual(Extra, type(extra), 'la lista contiene elementos que no son del tipo extra')
+
+    def test_agregar_cliente(self):
+        nuevo_cliente = Cliente(nombre='prueba', apellido='ordonez')
+        self.assertTrue(nuevo_cliente != None)
+        self.assertEqual(nuevo_cliente.nombre, 'prueba', 'deberia ser el mismo nombre')
+        self.assertEqual(nuevo_cliente.apellido, 'ordonez', 'deberia ser el mismo apellido')
+        cliente2 = Cliente('Alejando', 'ordonez')
+        cliente2.telefono = '484934304343'
+        self.assertEqual(cliente2.telefono , '484934304343', 'deberia poder pasar el telefono' )
+        # self.assertEqual(nuevo_c)
+        # cliente2 = Cliente(nombre="segundo")
+        #
+
+    def test_venta(self):
+        new_cliente = Cliente('alejandro', 'ordonez')
+        cocina = Mueble('cocina', 'larga',9)
+        nueva_venta = Venta()
+        nueva_venta.poner_cliente(new_cliente) # setear
+        nueva_venta.poner_mueble(cocina)
+        nueva_venta.poner_motivo('renovacion')
+        nueva_venta.poner_precio(4000.50)
+        nueva_venta.adelanto = 2000.50
+        self.assertEqual(nueva_venta.calcular_saldo(), 2000.00, 'no resto el adelanto de la venta')
+        # self.assertEqual(nueva_venta.ingreso , datetime.today())
+
+    def test_listar_ventas(self):
+        jairo = Cliente('Jairo', 'Ordonez')
+        ale = Cliente('alejandro', 'Ordonez')
+        cocina = Mueble('cocina', 'cocina moderna',100)
+        cama = Mueble('cama', 'Cama de dos plaza',200)
+
+        registro_de_ventas = RegistroDeVentas()
+
+        nueva_venta = Venta()
+        nueva_venta.poner_cliente(jairo)
+        nueva_venta.poner_mueble(cocina)
+        registro_de_ventas.agregar_venta(nueva_venta)
+
+        otra_venta = Venta()
+        otra_venta.poner_cliente(ale)
+        otra_venta.poner_mueble(cama)
+
+        registro_de_ventas.agregar_venta(otra_venta)
+        self.assertEqual(2, registro_de_ventas.devolver_cantidad_ventas())
+        valor_esperado = '\n'.join(['Nombre completo: Jairo Ordonez, Mueble: Cocina',
+                                   'Nombre completo: alejandro Ordonez, Mueble: cama'])
+        from io import StringIO
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            registro_de_ventas.listar_ventas()
+            self.assertEqual(fake_out.getvalue(), valor_esperado)
+
+    # # crear un test, que se llama test_devolver_costo_de_mueble
+    # # el test. debe instanciar un mueble, agregarle
+    # # 2  piezas, 2 extra . con sus respectivo precio
+
+    #
+
+    def test_devolver_costo_de_mueble(self):
+        cocina = Mueble('cocina', '1.20, 4 cajones',1500)
+        frente_cajon = Pieza(15, 45, 2, 'frente cajon',5)
+        costado_cajon = Pieza(15, 45, 2, 'costado cajon',5)
+        guia = Extra('guia_cajon','telescopica_45', 15)
+        tornillo = Extra('tornillo','telescopica_45', 15)
+
+        cocina.agregar_pieza(frente_cajon)
+        cocina.agregar_pieza(costado_cajon)
+        cocina.agregar_extra(guia)
+        cocina.agregar_extra(tornillo)
+
+        #e implementar un metodo en mueble que
+        # calcule el precio del mueble, para eso
+        # debe sumar el precio ,de todos sus piezas y
+        # sus extras
+
+        self.assertEqual(40,cocina.devolver_costo_de_mueble(),'no se calculo bien el costo')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+#
+# """ale = Cliente('alejandro', 'Ordonez','cajonera','12/1/2022',27/12/2022 )
+# thiago = Cliente('thiago','ordonez','cama',1/3/2020,25/3/2022)
+# juli = Cliente ('juli','ordonez','placard',5/8/2020,5/4/2022)"""
+
+# crear un meteodo en la clase RegistroDeVenta
+# llamado , devolver_las_ventas_de , y que tome
+# como parametro , el nombre del cliente , y apellido,
+# y devuelva , una lista con las ventas que coincidan
+# con en el nombre y apellid
+
+# Definir un metodo _eq_ en la clase cliente,
+# que tome como parámetro , otro cliente,
+# y devuelve True si  el nombre y apellido del cliente
+# es igual al otro cliente
+
+# definir metodo en la clase RegistroDeCliente
+# llamada agregar_cliente_input
+
+# Jairo ordoñez21:44
+# esta funcion no toma parametro (solo el self),
+# y pide al usuario un nombre(usar la funcion input),
+# un apellido, edad, y crea una instancia de cliente,
+# y luego usa la funcion agregar_cliente con la nueva
+# instancia creada
+
+# crear un metodo en la calse REgistro cliente,
+# que se llame guardar_cliente, que no tome argumento,
+# y  guarde el atributo, lista_cliente en un json ,
+# crear un metodo en la clase registtro_cleite,
+# que se lla,e cargar_Cleinte,
+# y que lea el json y lo asigne a lista_cliete
+
+# cocina = Mueble('cocina','1.20, 4 cajones',122)
+# frente_cajon = Pieza(15,45,2)
+# costado_cajon = Pieza(11,45,2)
+# contra_frente = Pieza(7,45,2)
+
+# cocina = Mueble('cocina','1.20, 4 cajones',122)
+# frente_cajon = Pieza(15,45,2,'frente cajon')
+# costado_cajon = Pieza(11,45,2,'costado cajon')
+# contra_frente = Pieza(7,45,2,'contra frente')
+
+# crear una funcion llamada sumatoria, que tome
+# una lista de numeros, y sume su total
+#
+# ejemplo
+# sumatoria([1,2,2,5])
+# == 10
+#
+# sumatora([])
+# ==0
+#
+#
+# crear una funcion llamada productoria. que tome
+# una lista de numeros enteros y devueva el producto
+# de todos sus numeros
+#
+#
+# ejemplos
+# productoria([1,1,1,1])
+#  == 1
+# productoria([1,2,3,4])
+# 24
+
+# recorre la lista
+# compara 2 elementos
+#lo guarda en 1 lista
+# vuelve a comparar
+# si el elemento es menor al ya seleccionado termina
+
+# toma una lista de elementos,
+# devueleve 2 listas de elementos
+# ina lista con elmentos pares
+# la otra con los impares
+
+# crear un test, que se llama test_precio_mueble
+# el test. debe instanciar un mueble, agregarle
+# 2  piezas, 2 extra . con sus respectivo precio
